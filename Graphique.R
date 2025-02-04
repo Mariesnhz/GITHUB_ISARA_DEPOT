3dc563cafe2a0de8c415bb1bb407c445b3b9e647
Marie <msanchez@etu.isara.fr>
2025-02-04 11:41:16 +0100

    graphique

---
+++ Graphique.R
@@ -0,0 +1,15 @@
+####prise en main / tests de graphiques####
+
+
+##ggplot2
+install.packages("ggplot2")
+library(ggplot2)
+
+CO2
+names(CO2)
+
+ggplot(CO2, aes(conc, uptake,
+             colour = Treatment))+
+  geom_point(size = 3, alpha = 0.5)+
+  geom_smooth()
+
