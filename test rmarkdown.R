983bfa971197c1e4d2ebb87b28285832b07d9c73
Marie <msanchez@etu.isara.fr>
2025-02-04 17:15:22 +0100

    rapport graphique

---
+++ test rmarkdown.R
@@ -0,0 +1,17 @@
+---
+  title: "Rapport test Rmarkdown"
+author: "Sanchez Marie-sarah"
+date: "`r format(Sys.time(), '%d %B %Y')`"
+output:
+  html_document:
+  code_folding: hide
+fig_caption: yes
+number_sections: yes
+toc: yes
+toc_depth: 3
+toc_float:
+  collapsed: true
+smooth_scroll: true
+
+bibliography: biblio/biblio2.bib
+csl: biblio/isara-iso-690.csl
