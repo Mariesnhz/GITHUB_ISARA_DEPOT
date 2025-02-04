c68969b04e7fd4c2515fac93339c8de5bc8e6121
Marie <msanchez@etu.isara.fr>
2025-02-04 12:05:43 +0100

    test graphique

--- test_graphique1.R
+++ test_graphique1.R
@@ -9,9 +9,7 @@ CO2
 names(CO2)
 
 ggplot(CO2, aes(conc, uptake,
-             colour = Treatment))+
+                colour = Treatment))+
   geom_point(size = 3, alpha = 0.5)+
   geom_smooth(color = "pink")
 
-
-  
